416a0f3b8bdc19d2bc8caefb8673b090821ce5e0
U-C-S <uchanakyasrinivas@gmail.com>
2022-11-06 01:08:20 +0530

    add window_vibrancy dep for mica material

--- core/src/main.rs
+++ core/src/main.rs
@@ -4,6 +4,8 @@
 )]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use tauri::Manager;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -11,6 +13,13 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+
+            window_vibrancy::apply_mica(&window).expect("ERROR");
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
