ee0f95d9eddf55644d6fe1e96d40f3477df62ef7
U-C-S <uchanakyasrinivas@gmail.com>
2022-11-06 01:13:27 +0530

    Fix mica issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,14 @@ import type { AppProps } from "next/app";
 
 import "../style.css";
 import "../App.css";
+import { useEffect } from "react";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    import("@tauri-apps/api/window").then((tauri) => {
+      tauri.appWindow.setDecorations(true);
+    });
+  });
   return <Component {...pageProps} />;
 }
