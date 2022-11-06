50da5a9d2f7ce0367d5d8c218b5d1fb5bd5fbc1f
U-C-S <uchanakyasrinivas@gmail.com>
2022-11-06 23:28:28 +0530

    Update Frontend to use Mantine and removed CSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,42 @@
 import type { AppProps } from "next/app";
-
-import "../style.css";
-import "../App.css";
 import { useEffect } from "react";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
   useEffect(() => {
     import("@tauri-apps/api/window").then((tauri) => {
       tauri.appWindow.setDecorations(true);
     });
   });
-  return <Component {...pageProps} />;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          globalStyles: (theme) => ({
+            body: {
+              backgroundColor: "transparent",
+            },
+          }),
+          black: "#00000000",
+          colorScheme: "dark",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,34 @@ import Image from "next/image";
 import reactLogo from "../assets/react.svg";
 import tauriLogo from "../assets/tauri.svg";
 import mantineLogo from "../assets/mantine.svg";
+import {
+  Button,
+  Container,
+  createStyles,
+  Divider,
+  Group,
+  Stack,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  container: {
+    paddingTop: "10vh",
+  },
+
+  NextInfo: {
+    position: "absolute",
+    bottom: "10px",
+    width: "100%",
+  },
+}));
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const { classes } = useStyles();
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -15,68 +39,71 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1>Welcome !!</h1>
+    <>
+      <Container className={classes.container}>
+        <Stack align="center" spacing={25}>
+          <Title order={1}>Welcome !!!</Title>
+          <Group position="center" spacing={50}>
+            <span className="logos">
+              <a href="https://mantine.dev" target="_blank">
+                <Image
+                  width={84}
+                  height={84}
+                  src={mantineLogo}
+                  className="logo mantine"
+                  alt="Mantine logo"
+                />
+              </a>
+            </span>
+            <span className="logos">
+              <a href="https://tauri.app" target="_blank">
+                <Image
+                  width={84}
+                  height={84}
+                  src={tauriLogo}
+                  className="logo tauri"
+                  alt="Tauri logo"
+                />
+              </a>
+            </span>
+            <span className="logos">
+              <a href="https://reactjs.org" target="_blank">
+                <Image
+                  width={84}
+                  height={84}
+                  src={reactLogo}
+                  className="logo react"
+                  alt="React logo"
+                />
+              </a>
+            </span>
+          </Group>
+          <Text>
+            Click on the Mantine, Tauri and React logos to learn more.
+          </Text>
 
-      <div className="row">
-        <span className="logos">
-          <a href="https://mantine.dev" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={mantineLogo}
-              className="logo mantine"
-              alt="Mantine logo"
+          <Divider w={500} />
+
+          <Group position="center">
+            <TextInput
+              placeholder="Enter your name"
+              onChange={(e) => setName(e.currentTarget.value)}
             />
-          </a>
-        </span>
-        <span className="logos">
-          <a href="https://tauri.app" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={tauriLogo}
-              className="logo tauri"
-              alt="Tauri logo"
-            />
-          </a>
-        </span>
-        <span className="logos">
-          <a href="https://reactjs.org" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={reactLogo}
-              className="logo react"
-              alt="React logo"
-            />
-          </a>
-        </span>
-      </div>
+            <Button onClick={() => greet()} variant="default">
+              Greet
+            </Button>
+          </Group>
 
-      <p>Click on the Mantine, Tauri and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
-        </div>
-      </div>
-
-      <p>{greetMsg}</p>
-
-      <div className="nextjstext row">
-        <p>
-          The frontend is powered with <a href="https://nextjs.org" target="_blank">Next.js</a>
-        </p>
-      </div>
-    </div>
+          <Text>{greetMsg}</Text>
+        </Stack>
+      </Container>
+      <Text align="center" className={classes.NextInfo}>
+        The frontend is powered with{" "}
+        <a href="https://nextjs.org" target="_blank">
+          Next.js
+        </a>
+      </Text>
+    </>
   );
 }
 
